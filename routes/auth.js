7dee247fe3c53c99b2fce59d57158dbe6c55ca36
Quadzal <40356538+Quadzal@users.noreply.github.com>
2019-08-19 16:02:09 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authorModel = require("../models/Author");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 router.post("/register", async (req, res) => {
     try {
