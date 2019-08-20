070dc83d23ebc39653eaa5981eb7211ab3a09828
Quadzal <esat3515@gmail.com>
2019-08-20 20:43:29 +0300

    Updated
    Login And Register View Updated.

--- src/components/user/login.vue
+++ src/components/user/login.vue
@@ -5,7 +5,8 @@ export default {
             author:{
                 username:"",
                 password:""
-            }
+            },
+            error_message:""
         }
     },
 
@@ -16,7 +17,7 @@ export default {
                 this.$router.push("/");
             })
             .catch((err) => {
-                console.log(err)
+                this.error_message = "Dont Success Login."
             })
         }
     }
@@ -24,19 +25,74 @@ export default {
 </script>
 
 <template>
-    <b-container>
-        <b-row class="mx-auto">
-            <b-col cols="3"></b-col>
-            <b-col cols="6" >
-                <b-form @submit.prevent="login">
-                    <label>Username</label>
-                    <b-form-input type="text" v-model="author.username"></b-form-input>
-                    <label>Password</label>
-                    <b-form-input type="password" v-model="author.password"></b-form-input>
-                    <b-button variant="danger" class="mt-2 w-50" type="submit">Login</b-button>
-                </b-form>
-            </b-col>
-            <b-col cols="3"></b-col>
-        </b-row>
-    </b-container>
+    <div class="background">
+        <b-container>
+            <b-row class="mx-auto">
+                <b-col cols="3"></b-col>
+                <b-col cols="5" class="text-center">
+                    <b-alert v-if="error_message" class="alert w-100" show> {{ error_message }} </b-alert>
+                </b-col>
+                
+                <b-col cols="4"></b-col>
+                <b-col cols="3"></b-col>
+                <b-col cols="5" class="text-white">
+                    <b-form @submit.prevent="login">
+                        <label>Username</label>
+                        <b-form-input type="text" v-model="author.username" required placeholder="Username..."></b-form-input>
+                        <label>Password</label>
+                        <b-form-input type="password" v-model="author.password" required placeholder="Password..."></b-form-input>
+                        <b-button class="mt-3 login-button w-100" type="submit">Login</b-button>
+                    </b-form>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
 </template>
+
+<style scoped>
+    .background{
+        height: 100%;
+        background-image: url("../../assets/background-user.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center center;
+        background-attachment: fixed;
+        display:flex;
+        align-items: center;
+        justify-content:center;
+    }
+
+    .login-button{
+        background-color: #5f8985;
+        border-radius: 20px;
+        border-color: #34394d;
+    }
+
+    .login-button:hover{
+        box-shadow:0 0 0 0.1em #34394d;
+    }
+
+    .alert{
+        color: #ffffff;
+        background-color: #71ab9c;
+    }
+
+    input{
+        background-color: transparent;
+        color: white;
+        border-color: #34394d;
+        border-width: 2px;
+        border-radius: 20px;
+    }
+
+    input:focus{
+        background-color: transparent;
+        color:white;
+        box-shadow:0 0 0 0.1rem #34394d;
+        border-color: #34394d;
+    }
+
+    ::placeholder{
+        color: white;
+    }
+</style>
--- src/components/user/register.vue
+++ src/components/user/register.vue
@@ -8,7 +8,8 @@ export default {
                 email:"",
                 first_name:"",
                 last_name:""
-            }
+            },
+            error_message:""
         }
     },
 
@@ -19,7 +20,7 @@ export default {
                 this.$router.push("/");
             })
             .catch((err) => {
-                console.log(err)
+                this.error_message = "Dont Success Register."
             })
         }
     }
@@ -27,25 +28,80 @@ export default {
 </script>
 
 <template>
-    <b-container>
-        <b-row class="mx-auto">
-            <b-col cols="3"></b-col>
-            <b-col cols="6" >
-                <b-form @submit.prevent="register">
-                    <label>First Name</label>
-                    <b-form-input type="text" v-model="author.first_name" required></b-form-input>
-                    <label>Last Name</label>
-                    <b-form-input type="text" v-model="author.last_name" required></b-form-input>
-                    <label>Username</label>
-                    <b-form-input type="text" v-model="author.username" required></b-form-input>
-                    <label>Email</label>
-                    <b-form-input type="email" v-model="author.email" required></b-form-input>
-                    <label>Password</label>
-                    <b-form-input type="password" v-model="author.password" required></b-form-input>
-                    <b-button variant="danger" class="mt-2 w-50" type="submit">Register</b-button>
-                </b-form>
-            </b-col>
-            <b-col cols="3"></b-col>
-        </b-row>
-    </b-container>
+    <div class="background">
+        <b-container>
+            <b-row class="mx-auto">
+                <b-col cols="3"></b-col>
+
+                <b-col cols="5" class="text-center">
+                    <b-alert v-if="error_message" class="alert" show> {{ error_message }} </b-alert>
+                </b-col>
+
+                <b-col cols="4"></b-col>
+                <b-col cols="3"></b-col>
+                <!-- Register Form -->
+
+                <b-col cols="5" >
+                    <b-form @submit.prevent="register" class="text-white">
+                        <label>First Name</label>
+                        <b-form-input type="text" v-model="author.first_name" required placeholder="First Name..."></b-form-input>
+                        <label>Last Name</label>
+                        <b-form-input type="text" v-model="author.last_name" required placeholder="Last Name..."></b-form-input>
+                        <label>Username</label>
+                        <b-form-input type="text" v-model="author.username" required placeholder="Username..."></b-form-input>
+                        <label>Email</label>
+                        <b-form-input type="email" v-model="author.email" required placeholder="Email..."></b-form-input>
+                        <label>Password</label>
+                        <b-form-input type="password" v-model="author.password" required placeholder="Password..."></b-form-input>
+                        <b-button class="mt-3 w-100 register-button" type="submit">Register</b-button>
+                    </b-form>
+                </b-col>
+                <!-- ./Register Form -->
+
+            </b-row>
+        </b-container>
+    </div>
 </template>
+
+<style scoped>
+    .background{
+        height: 100%;
+        background-image: url("../../assets/background-user.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center center;
+        background-attachment: fixed;
+        display:flex;
+        align-items: center;
+    }
+    
+    .register-button{
+        background-color: #5f8985;
+        border-radius: 20px;
+        border-color: #34394d;
+    }
+    
+    .register-button:hover{
+        box-shadow:0 0 0 0.2em #34394d;
+    }
+    .alert{
+        color: #ffffff;
+        background-color: #71ab9c;
+    }
+    input{
+        background-color: transparent;
+        color: white;
+        border-color: #34394d;
+        border-width: 2px;
+        border-radius: 20px;
+    }
+    input:focus{
+        background-color: transparent;
+        color:white;
+        box-shadow:0 0 0 0.1em #34394d;
+        border-color: #34394d;
+    }
+    ::placeholder{
+        color: white;
+    }
+</style>
