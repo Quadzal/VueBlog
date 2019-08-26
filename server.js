657b505f7883bf7256d12ececbbaae68cbfb90e2
Quadzal <esat3515@gmail.com>
2019-08-26 16:09:36 +0300

    commits

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const auth = require("./server/routes/auth");
 const author = require("./server/routes/author");
 const category = require("./server/routes/category");
 const article = require("./server/routes/article");
-const config = require("./server/config");
 
-config.setConfig(app);
+app.use(history());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.set("json spaces", 4)
+app.use(cors());
+app.use(express.static(__dirname));
 
 database.connect_mongodb();
 
